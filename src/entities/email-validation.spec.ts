42d34800d8832a6a6b77459dcf358024157baff1
Waldir Junior <wjunior@ciandt.com>
2022-07-14 08:05:11 -0300

    test: validate email with domain part with invalide char

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -45,4 +45,9 @@ describe('Email validation', () => {
     const email = 'any@' + 'd'.repeat(64) + '.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept local part with invalid char', () => {
+    const email = 'any email@email.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })

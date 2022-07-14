8f85497fdd22e526e41d05b4cf5428d80cfcdc59
Waldir Junior <wjunior@ciandt.com>
2022-07-14 07:40:59 -0300

    test: validate emails with more than 320 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -16,6 +16,11 @@ describe('Email validation', () => {
     expect(Email.validate(email)).toBeTruthy()
   })
 
+  test('should not accept larger than 320 chars', () => {
+    const email = 'c'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
   test('should not accept local part larger than 64 chars', () => {
     const email = 'l'.repeat(65) + '@email.com'
     expect(Email.validate(email)).toBeFalsy()

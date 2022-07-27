6914967c2fa74665ddd7f7a16c92b526d48f3c23
Waldir Junior <wjunior@ciandt.com>
2022-07-26 23:08:11 -0300

    test: verify name while creating user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,6 +1,7 @@
 import { User } from './user'
 import { left } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
 
 describe('User domain entity', () => {
   test('should not create user with invalid e-mail address', () => {
@@ -8,5 +9,19 @@ describe('User domain entity', () => {
     const error = User.create({ name: 'any_name', email: invalidEmail })
 
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('should not create user with invalid name (too few characters)', () => {
+    const invalidName = '0    '
+    const error = User.create({ name: invalidName, email: 'any@mail.com' })
+
+    expect(error).toEqual(left(new InvalidNameError()))
+  })
+
+  test('should not create user with invalid name (too many characters)', () => {
+    const invalidName = '0'.repeat(257)
+    const error = User.create({ name: invalidName, email: 'any@mail.com' })
+
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })

12a2a5fd504fad4d1f8ef90eab0aaf9dda3c3ddb
Waldir Junior <wjunior@ciandt.com>
2022-08-14 10:17:11 -0300

    feat: web controller should return 500 when server raises

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,32 +1,36 @@
-import { MissingParamError } from '@/web-controllers/errors/missing-param-error';
+import { UseCase } from '@/usecases/ports'
+import { MissingParamError } from '@/web-controllers/errors/missing-param-error'
 import { UserData } from '@/entities'
 import { HttpResponse, HttpRequest } from '@/web-controllers/ports'
-import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
-import { badRequest, created } from '@/web-controllers/util'
+import { badRequest, created, serverError } from '@/web-controllers/util'
 
 export class RegisterUserController {
-  private readonly usecase: RegisterUserOnMailingList
+  private readonly usecase: UseCase
 
-  constructor (usecase: RegisterUserOnMailingList) {
+  constructor (usecase: UseCase) {
     this.usecase = usecase
   }
 
   public async handle (request: HttpRequest): Promise<HttpResponse> {
-    if (!(request.body.name) || !(request.body.email)) {
-      let missingParam = !(request.body.name) ? 'name ' : ''
-      missingParam += !(request.body.email) ? 'email' : ''
-      return badRequest(new MissingParamError(missingParam.trim()))
-    }
+    try {
+      if (!(request.body.name) || !(request.body.email)) {
+        let missingParam = !(request.body.name) ? 'name ' : ''
+        missingParam += !(request.body.email) ? 'email' : ''
+        return badRequest(new MissingParamError(missingParam.trim()))
+      }
 
-    const userData: UserData = request.body
-    const response = await this.usecase.registerUserOnMailingList(userData)
+      const userData: UserData = request.body
+      const response = await this.usecase.perform(userData)
 
-    if (response.isLeft()) {
-      return badRequest(response.value)
-    }
+      if (response.isLeft()) {
+        return badRequest(response.value)
+      }
 
-    if (response.isRight()) {
-      return created(response.value)
+      if (response.isRight()) {
+        return created(response.value)
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
